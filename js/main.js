c3d6710a108b0467d2075287593b114be8b64efe
Anna Nilsson <anna.nilsson@cmeducations.se>
2022-01-31 11:17:02 +0100

    js clean up

--- js/main.js
+++ js/main.js
@@ -1,45 +1,51 @@
 
-//Ingen loop ska behövas för sysslorna att dyka upp i flertal
-// TextField som tom string?
-
-
-
-
-
-
 //When clicking Add-btn:
 let addBtn = document.getElementById('add-btn');
 addBtn.addEventListener('click', function(e) {
 
+    //Error message when trying to sumbit empty text field
+    let textField = document.getElementById('text-field')
+    let existingErrorMessage = document.getElementById('error-message')
+    console.log(existingErrorMessage)
+    if(existingErrorMessage){
+     existingErrorMessage.remove()
+    }
 
+    if (textField.value === '') {
+       let errorMessage = document.createElement('div')
+        errorMessage.id = "error-message";
+        errorMessage.innerText= `A task can't be empty`;
+        document.getElementById('fill-in-container').append(errorMessage);
+
+    } else {
+
+//Adds task to task list, creates "task row"- elements
 let ulTaskList = document.getElementById('ul-task-list');
 
-
+//
 let liTaskList = document.createElement('li');
 liTaskList.id = "li-task-list";
 
 ulTaskList.append(liTaskList);
 
-
-
+//
 let inputCheckbox = document.createElement('input');
 inputCheckbox.id = "input-checkbox";
 inputCheckbox.setAttribute('type', 'checkbox')
 
 liTaskList.append(inputCheckbox)
 
-
+//
 let inputTextTaskList = document.createElement('input');
 inputTextTaskList.id = "input-text-task-list";
 
-let textField = document.getElementById('text-field')
 inputTextTaskList.value = textField.value; 
 textField.value = "";
 inputTextTaskList.disabled = true;
 
 liTaskList.append(inputTextTaskList)
 
-
+//
 let changeBtn = document.createElement('button');
 changeBtn.id = "change-btn";
 changeBtn.innerHTML = `Change`;
@@ -59,6 +65,7 @@ changeBtn.addEventListener('click', function(e) {
 
 })
 
+//
 let deleteBtn = document.createElement('button');
 deleteBtn.id = "delete-btn";
 deleteBtn.innerHTML =`Delete`;
@@ -71,11 +78,12 @@ deleteBtn.addEventListener('click', function(e) {
 
 })
 
+//
 let ulDoneList = document.getElementById('ul-done-list');
 
+//
 inputCheckbox.addEventListener('click', function(e) {
 
-    // let liDoneList = liTaskList;
     
     let clickedBox = e.target;
     clickedBox.parentNode.remove();
@@ -83,86 +91,20 @@ inputCheckbox.addEventListener('click', function(e) {
     ulDoneList.append(liTaskList, inputCheckbox, inputTextTaskList, changeBtn, deleteBtn);
     
 })
+
+// Reset all button
 let resetBtn = document.getElementById('reset-btn');
 resetBtn.addEventListener('click', function(e) {
     let clickedBtn = e.target;
-    // ulDoneList.parentNode.remove();
     liTaskList.parentNode.remove();
     clickedBtn.parentNode.innerHTML = `Your list is empty!`;
     
 })
-
+}
 })
 
 
 
 
-// OBS! textField.value //refererar till texten innanför textfältet (saknar sluttag)
 
 
-
-//delete-btn and change-btn 
-
-// let deleteBtn = document.getElementById('delete-btn');
-// deleteBtn.addEventListener('click', function(e) {
-//     let clickedBtn = e.target;
-//     clickedBtn.parentNode.remove();
-// })
-
-// let addedTask = document.getElementById('added-field');
-// let changeBtn = document.getElementById('change-btn');
-// changeBtn.addEventListener('click', function(e) {
-//     let clickedBtn = e.target;
-//     clickedBtn.parentNode.remove();
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let newTask = document.getElementById("task-row");
-// let finishedTasks = document.getElementById("done-row");
-
-// for (let i = 0; i < newTask.length; i++) {
-//     addBtn.addEventListener('click', function(e) {
-//         let clickedBox = e.target;
-//         clickedBox.child[1].enable;
-
-// });
-
-// let changeBtn = document.getElementsByClassName('change-btn');
-// changeBtn.addEventListener('click', function(e) {
-//     let clickedBox = e.target;
-//     clickedBox.child[1].enable;
-// })
-
-
-//Varför funkar inte eventlistener på mina classer? 
-
-// let deleteBtn2 = document.getElementsByClassName('delete-btn2');
-// deleteBtn2.addEventListener('click', function(e) {
-//     let clickedBox = e.target;
-//     clickedBox.parentNode.remove();
-//})
-
-
-
-
-// Reset button (VG-del)
-// let resetBtn = document.getElementById('reset-btn');
-// resetBtn.addEventListener('click', function(e) {
-//     clickedBox = e.target;
-//     finishedTasks.remove();
-//     taskRow.remove();
-// })
